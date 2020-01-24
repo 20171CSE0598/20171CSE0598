9d97dff5899368cda6e8d5bb7ac696e0ebaf04c0
20171CSE0598 <60249425+20171CSE0598@users.noreply.github.com>
2020-01-24 10:53:57 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter the value of a and b\n");
+scanf("%d%d",&a,&b);
+printf("a=%d\n",a);
+printf("b=%d\n",b);
+printf("a value is %d and b value is %d\n",a,b);
+printf("%d,%d\n",a,b);
+}
